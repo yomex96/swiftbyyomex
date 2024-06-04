3a8c1acdf3c77f4fa3f44192319df42f1814662e
yomex96 <abayomirobertonawole2@gmail.com>
2024-06-04 06:00:10 +0100

    okay

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import { useState } from 'react'
 import LoginPopup from './components/LoginPopup/LoginPopup'
+// import Verify from './pages/Verify/Verify'
 import Verify from './pages/Verify/Verify'
 import MyOrders from './pages/MyOrders/MyOrders'
 import About from './pages/About/About'
@@ -37,6 +38,7 @@ const App = () => {
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
           <Route path='/verify' element={<Verify />} />
+          {/* <Route path='/verify' element={<Verify />} /> */}
           <Route path='/myorders' element={<MyOrders />} />
           < Route path='/About' element={<About/>} />
         </Routes>
